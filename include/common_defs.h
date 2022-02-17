0beba81855a0aa0c06518264671bc99142957647
Ayush <ayush.sriv79@gmail.com>
2022-02-17 01:38:59 -0800

    common_defs: macros updated for timers

---
+++ include/common_defs.h
@@ -0,0 +1,8 @@
+#ifndef __COMMON_DEFS_H__
+#define __COMMON_DEFS_H__
+
+#define TIMER_DEFAULT_INTERVAL     1
+#define TIMER_MULTIPLIER           1000000
+#define TIMER_CALCULATOR(interval) (interval * TIMER_MULTIPLIER)
+
+#endif //__COMMON_DEFS_H__
