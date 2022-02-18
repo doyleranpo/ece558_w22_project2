3094500271b11dd04673954104b8efc90f2362a4
Ayush <ayush.sriv79@gmail.com>
2022-02-18 15:54:11 -0800

    button: initial implementation for button

---
+++ include/button.h
@@ -0,0 +1,9 @@
+#ifndef __BUTTON_H__
+#define __BUTTON_H__
+#include <Arduino.h>
+
+void button_setup();
+void read_button();
+bool is_pressed();
+
+#endif //__BUTTON_H__
---
+++ src/button.cpp
@@ -0,0 +1,31 @@
+#include "button.h"
+#include "common_defs.h"
+
+static unsigned long hit_time = 0; 
+static bool button_status = false;
+static bool last_state = false;
+
+void button_setup() {
+    pinMode(BUTTON_PIN, INPUT_PULLDOWN);
+}
+
+bool is_pressed() {
+    return button_status;
+}
+
+void read_button() {  
+    bool read_pin = digitalRead(BUTTON_PIN);
+
+    if (read_pin != last_state) {
+        hit_time = millis();
+    }
+    
+    if (millis() - hit_time > BUTTON_DEBOUNCE_MS) {
+        if(button_status != read_pin) {
+            button_status = read_pin;
+            Serial.println("Button Pressed");
+        }
+    }
+
+    last_state = read_pin;
+}
