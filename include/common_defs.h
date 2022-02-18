c9db2439353d06b4bba04d9a9aedc4aa29a41f2a
Ayush <ayush.sriv79@gmail.com>
2022-02-18 15:53:20 -0800

    commondefs: updated for push button

--- include/common_defs.h
+++ include/common_defs.h
@@ -1,8 +1,14 @@
 #ifndef __COMMON_DEFS_H__
 #define __COMMON_DEFS_H__
 
-#define TIMER_DEFAULT_INTERVAL     1
-#define TIMER_MULTIPLIER           1000000
-#define TIMER_CALCULATOR(interval) (interval * TIMER_MULTIPLIER)
+#define TIMER_DEFAULT_INTERVAL      1
+#define TIMER_MULTIPLIER            1000000
+#define TIMER_CALCULATOR(interval)  (interval * TIMER_MULTIPLIER)
+
+#define BUTTON_PIN                  33
+#define BUTTON_DEBOUNCE_MS          25          
+
+#define LED_PIN                     12
+
 
 #endif //__COMMON_DEFS_H__
